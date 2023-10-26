7404a55abf2782636eadb2236d73cd706ecdc75a
Luiz Fernando <luizfernando.cg@hotmail.com>
2023-10-26 12:21:24 -0300

    Used some bulma pre made css render to put in the box a list of all users when is in users route

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-user-list',
   template: `
-    <p>
-      user-list works!
-    </p>
+    <section class="section">
+      <div class="container">
+        <div class="columns is-multiline" *ngIf="users">
+          <div class="column is-4" *ngFor="let user of users | async">
+            <div class="card">
+              <div class="card-content">
+                {{ user.login }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   `,
-  styles: [
-  ]
+  styles: []
 })
 export class UserListComponent {
+  users: any;
 
+  constructor(private userService: UserService) {}
+  ngOnInit() {
+    this.users = this.userService.getUsers();
+  }
 }
